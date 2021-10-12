01477f318f1bab00aacbdc31abd8d5f691986a8e
Volodymyr Korenevskyy <korik_uman@yahoo.com>
2021-10-12 12:34:43 -0500

    WebTests project first commit

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebTest {
+    WebDriver driver;
+    String url = "http://www.99-bottles-of-beer.net/";
+
+    @BeforeTest
+    public void setDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vkorenevskyy\\IdeaProjects\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void quitDriver() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void testFirst() {
+
+        String expectedResult = "99 Bottles of Beer | Start";
+
+        driver.get(url);
+        driver.getCurrentUrl();
+
+        String actual = driver.getTitle();
+
+        Assert.assertEquals(actual, expectedResult);
+    }
+
+    @Test
+    public void testSearch() {
+
+        driver.get(url);
+
+        driver.findElement(By.xpath("(//a[normalize-space()='Search Languages'])[1]")).click();
+        driver.findElement(By.cssSelector("input[name='search']")).sendKeys("English");
+        driver.findElement(By.cssSelector("input[value='GO']")).click();
+
+        WebElement expectedResult = driver.findElement(By.xpath("(//th[normalize-space()='Language'])[1]"));
+
+        Assert.assertTrue(expectedResult.isDisplayed());
+    }
+}
